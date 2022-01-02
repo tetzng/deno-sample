1bd7bd26e5e64c68b1ac66c5359cc171b5fa1cff
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-02 22:38:31 +0900

    add echo func

--- denops/helloworld/main.ts
+++ denops/helloworld/main.ts
@@ -1,5 +1,12 @@
-import {Denops} from 'https://deno.land/x/denops_std@v1.0.0/mod.ts';
+import { Denops } from 'https://deno.land/x/denops_std@v1.0.0/mod.ts';
+import { ensureString } from 'https://deno.land/x/unknownutil@v1.0.0/mod.ts';
 
 export async function main(denops: Denops): Promise<void> {
-  console.log('Hello Denops!');
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<void> {
+    ensureString(text);
+    return await Promise.resolve(text);
+    },
+  };
 };
+
