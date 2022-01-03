9e2365ae3451d7ae6c716d8394c17ee1a2ddea2e
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-03 20:36:49 +0900

    add execute

--- denops/helloworld/main.ts
+++ denops/helloworld/main.ts
@@ -1,5 +1,6 @@
 import { Denops } from 'https://deno.land/x/denops_std@v1.0.0/mod.ts';
 import { ensureString } from 'https://deno.land/x/unknownutil@v1.0.0/mod.ts';
+import { execute } from 'https://deno.land/x/denops_std@v1.0.0/helper/mod.ts';
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
@@ -8,5 +9,10 @@ export async function main(denops: Denops): Promise<void> {
     return await Promise.resolve(text);
     },
   };
+
+  await execute(
+    denops,
+    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+  );
 };
 
